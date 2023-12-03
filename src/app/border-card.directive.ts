d8f62edef1da6576b7ea6629815404cff0131f60
Bertou19 <a_bertille@msn.comm>
2023-12-03 18:41:08 +0100

    pkmnBorderCard directive terminée

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,9 +1,28 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, HostListener } from "@angular/core";
 
 @Directive({
-  selector: "[appBorderCard]",
+  selector: "[pkmnBorderCard]",
   standalone: true,
 })
 export class BorderCardDirective {
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) {
+    this.setHeight(180);
+    this.setBorder("#f5f5f5");
+  }
+  @HostListener("mouseenter") onMouseEnter() {
+    this.setBorder("#009688");
+  }
+
+  @HostListener("mouseleave") onMouseLeave() {
+    this.setBorder("#f5f5f5");
+  }
+
+  private setHeight(height: number) {
+    //Acceder à l'élément natif du dom qu'on veut cibler
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+
+  private setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4x ${color}`;
+  }
 }
